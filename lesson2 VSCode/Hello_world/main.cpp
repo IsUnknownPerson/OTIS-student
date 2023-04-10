5a7ab25c8e96a5c12ae1acdaf99ea8136d9244bd
Илья <raxman_i@mail.ru>
2023-04-10 10:17:06 +0300

    lesson 2

--- lesson2 VSCode/Hello_world/main.cpp
+++ lesson2 VSCode/Hello_world/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "move_me_out.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    todo_move_me_out();
+
     return 0;
 }
---
+++ lesson2 VSCode/Hello_world/move_me_out.cpp
@@ -0,0 +1,7 @@
+#include "move_me_out.h"
+#include <iostream>
+
+void todo_move_me_out()
+{
+   std::cout << "MSVC, а на русском сможешь?\r\n";
+}
